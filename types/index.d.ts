a1b764e443de572f9bb236f320218b5e8311ae9a
Glen Whitney <glen@studioinfinity.org>
2022-03-21 07:43:27 -0700

    Refactor/simplify core cleanup (#2490)
    * refactor: don't simplify constants in simplifyCore
    
      Keeps the operation of simplifyCore cleanly separate from
      simplifyConstant.
    
    * fix; handle multiple consecutive operations in simplifyCore()
    
       Also adds support for logical operators.
       Resolves #2484.
    
    * feat: export simplifyConstant
    
      Now that simplifyCore does not do any constant folding, clients may
      wish to access that behavior via simplifyConstant. Moreover, exporting it
      makes it easier to use in custom rule lists for simplify().
    
      Also adds docs, embedded docs, and tests for simplifyConstant().
    
      Also fixes simplifyCore() on logical functions (they always return boolean,
      rather than "short-circuiting").
    
      Resolves #2459.

--- types/index.d.ts
+++ types/index.d.ts
@@ -815,6 +815,9 @@ declare namespace math {
      */
     simplify: Simplify
 
+    simplifyConstant(expr: MathNode | string, options?: SimplifyOptions);
+    simplifyCore(expr: MathNode | string, options?: SimplifyOptions);
+
     /**
      * Calculate the Sparse Matrix LU decomposition with full pivoting.
      * Sparse Matrix A is decomposed in two matrices (L, U) and two
@@ -3608,6 +3611,8 @@ declare namespace math {
      * Default value is 10000.
      */
     fractionsLimit?: number
+    consoleDebug?: boolean
+    context?: object
   }
 
   type SimplifyRule =
@@ -4133,7 +4138,8 @@ declare namespace math {
      */
     simplify(rules?: SimplifyRule[], scope?: object): MathJsChain
 
-    simplifyCore(expr: MathNode): MathNode
+    simplifyConstant(options?: SimplifyOptions): MathJsChain
+    simplifyCore(options?: SimplifyOptions): MathJsChain
 
     /**
      * Calculate the Sparse Matrix LU decomposition with full pivoting.
